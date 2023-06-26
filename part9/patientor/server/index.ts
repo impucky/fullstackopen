3f68b33083627d2e0c632429443287f7d13f882a
Raphaël Beaugiraud <raphael.bgrd@gmail.com>
2023-06-26 14:47:02 +0200

    part9 - patientor initial commit

---
+++ part9/patientor/server/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use((cors as (options: cors.CorsOptions) => express.RequestHandler)({}));
+
+const PORT = 3001;
+
+app.get("/api/ping", (_req, res) => {
+  console.log("someone pinged here");
+  res.send("pong");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
